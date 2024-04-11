7d74409ec0b8b4bfeb3ba1eab68f1ca070985ccc
Muhammet Hadi KAMAT <muhammedhhadikamat@gmail.com>
2024-04-11 10:22:07 +0300

    post http method edited

--- data/request/book_request.go
+++ data/request/book_request.go
@@ -4,7 +4,7 @@ type CreateBookRequest struct {
 	Name   string `validate:"required,min=2,max=30" json:"name"`
 	Page   int    `validate:"required,min=20,max=500" json:"page"`
 	Author string `validate:"required,min=10,max=30" json:"author"`
-	Status bool   `validate:"default:false" json:"status"`
+	Status bool   `json:"status"`
 }
 
 type UpdateBookRequest struct {
